f10ea97377759e57edd452e03b28c7c2d20f72c6
paveldeuce <deucela.pavel@gmail.com>
2021-12-29 21:51:29 +0300

    add favicon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import Express from 'express';
 import session from 'express-session';
+import favicon from 'serve-favicon';
 import bodyParser from 'body-parser';
 import methodOverride from 'method-override';
 import routes from './routes.js';
 
 const app = new Express();
 
+app.use(favicon('public/favicon.ico'));
 app.use(methodOverride('_method'));
 app.set('view engine', 'pug');
 app.use('/css', Express.static('node_modules/bootstrap/dist/css'));
